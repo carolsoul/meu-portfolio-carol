2d93540cf613d243e0086c3fe01bcc41c2a5eaad
Anna Caroline <carolannago2005@gmail.com>
2025-09-15 17:04:38 -0300

    refactor: ajustar comentários para maior clareza

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-// 1. Importe o BrowserRouter
 import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* 2. Envolva o seu componente <App /> com o <BrowserRouter> */}
     <BrowserRouter>
       <App />
     </BrowserRouter>
