742f710268a7ec71bca84dc21bbe50f3bec2d5e1
Anna Caroline <carolannago2005@gmail.com>
2025-09-15 17:04:30 -0300

    refactor: ajustar comentários para maior clareza

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import { useState } from 'react';
-// 1. Importe os componentes de roteamento
 import { Routes, Route } from 'react-router-dom';
 
 import LoadingPage from './components/LoadingPage/LoadingPage';
 import ScrollContainer from './ScrollContainer';
 import ProjectsPage from './components/ProjectsPage/ProjectsPage'; // Nossa nova página
 
-// Vamos criar um componente wrapper para a lógica de loading
+// componente wrapper para a lógica de loading
 const PortfolioHome = () => {
   const [isLoading, setIsLoading] = useState(true);
 
@@ -20,9 +19,7 @@ const PortfolioHome = () => {
 
 function App() {
   return (
-    // 2. O componente Routes gerencia qual Route será renderizada
     <Routes>
-      {/* 3. Cada Route mapeia um caminho (path) para um componente (element) */}
       <Route path="/" element={<PortfolioHome />} />
       <Route path="/projetos" element={<ProjectsPage />} />
     </Routes>
