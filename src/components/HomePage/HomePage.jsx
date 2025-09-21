573e2a3614c7f2e8cb526f64496c98689c1803b6
Anna Caroline <carolannago2005@gmail.com>
2025-09-21 18:45:37 -0300

    refactor: adicionar novo controle em dispositivos móveis

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -2,10 +2,24 @@ import styles from './HomePage.module.css';
 import Ballpit from './Ballpit';
 
 const HomePage = () => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <div className={styles.homeContainer}>
       <div className={styles.background}>
-        <Ballpit />
+        {!isMobile && <Ballpit />}
       </div>
       <div className={styles.content}>
         <h1 className={styles.name}>Carol <br />Sousa</h1>
